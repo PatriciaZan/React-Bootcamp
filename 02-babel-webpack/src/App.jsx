da506bcf2db63e4c7067d624d51565c4a3c64dd5
Patricia Zan <patricia.zan.oliveira@gmail.com>
2026-05-25 20:26:17 -0300

    Manually building the webpack and babel config files

---
+++ 02-babel-webpack/src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Projeto Webpack e Babel</h1>
+    </div>
+  );
+}
---
+++ 02-babel-webpack/src/index.js
@@ -0,0 +1,7 @@
+import App from "./App";
+
+const { createRoot } = require("react-dom/client");
+
+const root = createRoot(document.getElementById("root"));
+
+root.render(<App />);
